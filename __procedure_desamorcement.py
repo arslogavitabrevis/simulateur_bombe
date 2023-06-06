f2a12414c6aa65658984d0e394e1608eec162949
Raphael <raphael1grenier@gmail.com>
2023-06-05 20:37:54 -0400

    Corriger les numéro de pin

--- __procedure_desamorcement.py
+++ __procedure_desamorcement.py
@@ -184,8 +184,8 @@ class SequenceDesarmorcement:
 
         # Define the inputs pins
         self.__cable_coupes = 0
-        self.__pins = [Pin(5, Pin.IN, Pin.PULL_DOWN)
-                       for i in range(len(liste_des_etapes)+1)]
+        self.__pins = [Pin(i, Pin.IN, Pin.PULL_DOWN)
+                       for i in range(1,len(liste_des_etapes)+1)]
         for i, pin in enumerate(self.__pins):
             pin.irq(trigger=Pin.IRQ_FALLING,
                     handler=lambda pin, pin_num=i: self.__gpio_interrup_callback(pin, pin_num))
@@ -216,7 +216,7 @@ class SequenceDesarmorcement:
          nouveau_temps) = liste_des_etapes[self.__etape]
 
         # Si la pin a vérifier a été coupé
-        if self.__pins[pin_a_verifier].value() == False:
+        if self.__pins[pin_a_verifier-1].value() == False:
             print("pin {} déconnecté".format(pin_a_verifier))
             self.__etape += 1
             if self.__etape >= len(liste_des_etapes):
